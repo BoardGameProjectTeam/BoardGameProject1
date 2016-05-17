951ceea54db4c72d08774e58daeae3b10e57a344
shadowraive <coolcliff892@Yahoo.com>
2016-05-16 19:09:18 -0500

    added code to actionperformer

--- core/src/com/boardgame/game/Controllers/ActionPerformer.java
+++ core/src/com/boardgame/game/Controllers/ActionPerformer.java
@@ -1,8 +1,27 @@
 package com.boardgame.game.Controllers;
 
+import com.boardgame.game.BoardClasses.MainBoard;
+import com.boardgame.game.CardClasses.Card;
+import com.boardgame.game.SkillClasses.Skill;
+
 /**
  * Class to handle doing actions/attacls on the board
  * Created by Cliff on 5/15/2016.
  */
 public class ActionPerformer {
+    private MainBoard mainBoard;
+    public ActionPerformer(MainBoard mainBoard){
+        this.mainBoard = mainBoard;
+    }
+    public void useCard(Card card){
+
+    }
+    public void useSkill(Skill skill){
+
+    }
+
+    public void performAttack(){
+
+    }
+
 }
