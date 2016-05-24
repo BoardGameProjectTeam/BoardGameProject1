fc71feb93ffe7767c20fda9d4b4402f7ec2c7c51
Sairyou <romeosbjr@gmail.com>
2016-05-23 19:56:51 -0500

    added TestUI class

--- core/src/com/boardgame/game/states/TestUI.java
+++ core/src/com/boardgame/game/states/TestUI.java
@@ -1,6 +1,21 @@
 package com.boardgame.game.states;
 
-
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.RepeatAction;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Timer;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 /**
  * Created by Romeo on 5/23/2016.
